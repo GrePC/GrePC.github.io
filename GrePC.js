327b77c43355d9f0bfe1134bb3b158629937ce7d
GrePC <64420953+GrePC@users.noreply.github.com>
2021-05-31 17:06:13 -0500

    Fixed Bug in JS

--- GrePC.js
+++ GrePC.js
@@ -9,12 +9,12 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-optionsList.forEach(o => {
-    o.addEventListener("click", () => {
-        selected.innerHTML = o.querySelector("label").innerHTML;
-        optionsContainer.classList.remove("active");
-    });
-});
+// optionsList.forEach(o => {
+//     o.addEventListener("click", () => {
+//         selected.innerHTML = o.querySelector("label").innerHTML;
+//         optionsContainer.classList.remove("active");
+//     });
+// });
 
 document.addEventListener("DOMContentLoaded", function() {
     document.querySelector(".menu-btn").addEventListener("click", function() {
